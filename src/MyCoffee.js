f1e545199a4872360074ea7665f17acb30ed3c0b
Sean Young <seanyoung1337@gmail.com>
2018-03-22 23:32:53 -0400

    Created UI for My Coffee screen that display a list of the users coffees. Also added react native elements library

--- src/MyCoffee.js
+++ src/MyCoffee.js
@@ -1,11 +1,43 @@
 import React, { Fragment } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import { Header, List, ListItem, Rating } from "react-native-elements";
+
+import my_coffees from "./my_coffees";
+
+// TO DO: hook up header buttons, add sorting, and search
 
 export default class MyCoffee extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>My brewed coffee</Text>
+        <Header
+          leftComponent={{ icon: "arrow-back", color: "#fff" }}
+          centerComponent={{ text: "My Coffee", style: { color: "#fff" } }}
+          rightComponent={{ icon: "search", color: "#fff" }}
+          outerContainerStyles={{ backgroundColor: "#8b8c8c" }}
+          innerContainerStyles={{ justifyContent: "space-between" }}
+        />
+        <List>
+          {my_coffees.map((coffee, index) => (
+            <ListItem
+              key={index}
+              title={coffee.name}
+              subtitle={
+                <View>
+                  <Text>{coffee.roaster}</Text>
+                  <Rating
+                    readonly
+                    fractions={1}
+                    type="heart"
+                    startingValue={coffee.rating}
+                    ratingColor="#8b8c8c"
+                    imageSize={15}
+                  />
+                </View>
+              }
+            />
+          ))}
+        </List>
       </View>
     );
   }
@@ -13,9 +45,6 @@ export default class MyCoffee extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
---
+++ src/my_coffees.js
@@ -0,0 +1,37 @@
+// This is a dummy list of coffees to build and test the "My Coffees" screen
+
+const my_coffees = [
+  {
+    roaster: "Phil & Sebastian",
+    name: "Gaharo Hill",
+    region: "Burundi",
+    type: "Light",
+    rating: 3.5,
+  },
+  {
+    roaster: "Phil & Sebastian",
+    name: "Gatuyaini",
+    region: "Kenya",
+    roast: "Light",
+    type: "Filter",
+    rating: 5,
+  },
+  {
+    roaster: "La Cabra",
+    name: "Pinheirnho",
+    region: "Brazil",
+    roast: "Light",
+    type: "Filter",
+    rating: 4,
+  },
+  {
+    roaster: "Smile Tiger",
+    name: "Nightmare Hippy Girl",
+    region: "Nicaragua",
+    roast: "Medium/Dark",
+    type: "Filter",
+    rating: 2,
+  },
+];
+
+export default my_coffees;
