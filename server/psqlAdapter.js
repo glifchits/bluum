9e8b7a1d07d0a727b4e18107f89a5f3296463db8
George Lifchits <george.lifchits@gmail.com>
2018-05-16 20:29:40 -0400

    pull BLUUM_RDS env var from .env

--- server/psqlAdapter.js
+++ server/psqlAdapter.js
@@ -1,6 +1,8 @@
 // https://medium.com/@james_mensch/node-js-graphql-postgresql-quickstart-91ffa4374663
 const pgPromise = require("pg-promise");
 
+require("dotenv").config();
+
 const connStr = process.env.BLUUM_RDS;
 const pgp = pgPromise({}); // empty pgPromise instance
 const psql = pgp(connStr); // get connection to your db instance
